71566b90b3585d029036b084577ec776e4601191
Your Name <stoy95536>
2023-09-19 17:11:32 +0800

    init

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from linebot.models import *
 import os
 import datetime
 import openai
-import googlemaps
-
-
 
 
 
@@ -37,23 +34,32 @@ def handle_message(event): # event.message.text 使用者輸入內容
         line_bot_api.reply_message(event.reply_token, message) 
         
     if event.message.text == '你好':
-        message = TextSendMessage(text=f"幹你娘\n時間{datetime.datetime.now()}") # bot return the Message to User
+        message = TextSendMessage(text=f"你好\n時間{datetime.datetime.now()}") # bot return the Message to User
         line_bot_api.reply_message(event.reply_token, message) 
     
     if '找地圖' in event.message.text:
         location = event.message.text.split(" ")[1]
         message = TextSendMessage(text=f"https://www.google.com/maps/search/?api=1&query={location}\n時間{datetime.datetime.now()}") # bot return the Message to User
         line_bot_api.reply_message(event.reply_token, message)
+        
+        
+    if '提醒' in event.message.text:
+        tip = event.message.text.split(" ")[1]
+        
+
+
+
+        
     
-    if event.message.text == '找地圖':
-        message = TextSendMessage(text=f"請問要找哪裡呢\n時間{datetime.datetime.now()}") # bot return the Message to User
-        line_bot_api.reply_message(event.reply_token, message) 
-        while 1:
-            location = event.message.text
-            if location != "":
-                message = TextSendMessage(text=f"https://www.google.com/maps/search/?api=1&query={location}\n時間{datetime.datetime.now()}") # bot return the Message to User
-                line_bot_api.reply_message(event.reply_token, message)
-                break 
+    # if event.message.text == '找地圖':
+    #     message = TextSendMessage(text=f"請問要找哪裡呢\n時間{datetime.datetime.now()}") # bot return the Message to User
+    #     line_bot_api.reply_message(event.reply_token, message) 
+    #     while 1:
+    #         location = event.message.text
+    #         if location != "":
+    #             message = TextSendMessage(text=f"https://www.google.com/maps/search/?api=1&query={location}\n時間{datetime.datetime.now()}") # bot return the Message to User
+    #             line_bot_api.reply_message(event.reply_token, message)
+    #             break 
 
 
 import os
