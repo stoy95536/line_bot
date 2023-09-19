423e9a3fbc453831a8d1463c19bfa3af83ac55ce
Your Name <stoy95536>
2023-09-19 14:58:52 +0800

    init

--- app.py
+++ app.py
@@ -40,6 +40,11 @@ def handle_message(event): # event.message.text 使用者輸入內容
         message = TextSendMessage(text=f"幹你娘\n時間{datetime.datetime.now()}") # bot return the Message to User
         line_bot_api.reply_message(event.reply_token, message) 
     
+    if '找地圖' in event.message.text:
+        location = event.message.text.split(" ")[1]
+        message = TextSendMessage(text=f"https://www.google.com/maps/search/?api=1&query={location}\n時間{datetime.datetime.now()}") # bot return the Message to User
+        line_bot_api.reply_message(event.reply_token, message)
+    
     if event.message.text == '找地圖':
         message = TextSendMessage(text=f"請問要找哪裡呢\n時間{datetime.datetime.now()}") # bot return the Message to User
         line_bot_api.reply_message(event.reply_token, message) 
