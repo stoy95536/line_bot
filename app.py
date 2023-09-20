a0d0a5e14da89ef2daa8c93226776e0819dbe84a
smileakk <91245678+smileakk@users.noreply.github.com>
2023-09-20 19:04:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ def handle_message(event): # event.message.text 使用者輸入內容
     #             break 
 
 
-    if event.message.text == 'fuck you':
-        message = TextSendMessage(text=f"幹你娘雞掰\n時間{datetime.datetime.now()}") # bot return the Message to User
+    if event.message.text == 'test':
+        message = TextSendMessage(text=f"test\n時間{datetime.datetime.now()}") # bot return the Message to User
         line_bot_api.reply_message(event.reply_token, message)
 
 import os
