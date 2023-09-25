88a20abbb2d9ea9c365cc343e47c2670f5fe990b
smileakk <williamliao2013@gmail.com>
2023-09-25 19:08:28 +0800

    test

--- rent_house.py
+++ rent_house.py
@@ -18,8 +18,8 @@ def administrative_district(event):
             alt_text = '按鈕樣板',
             template = ButtonsTemplate(
                 thumbnail_image_url='https://a.bbkz.net/guide/images/2/2d/%E5%8F%B0%E7%81%A3%E8%A1%8C%E6%94%BF%E5%8D%80%E5%9C%96.png',
-                title='請問你要找哪個縣市的房子呢？',
-                text='123',
+                title='請問你要找哪個區域的房子呢？',
+                text='也可直接在對話筐輸入區域',
                 actions=[
                     MessageTemplateAction(
                         label='六都',
