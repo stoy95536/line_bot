979f5df717b3afae8375405ef812571aff89296d
smileakk <williamliao2013@gmail.com>
2023-09-25 19:06:23 +0800

    test

--- rent_house.py
+++ rent_house.py
@@ -12,14 +12,14 @@ line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 #六都 離島 非六都選擇
-def administrative_district(event): #找租屋對話筐_選擇縣市
+def administrative_district(event): 
     try:
         message = TemplateSendMessage(
             alt_text = '按鈕樣板',
             template = ButtonsTemplate(
                 thumbnail_image_url='https://a.bbkz.net/guide/images/2/2d/%E5%8F%B0%E7%81%A3%E8%A1%8C%E6%94%BF%E5%8D%80%E5%9C%96.png',
                 title='請問你要找哪個縣市的房子呢？',
-                text='',
+                text='123',
                 actions=[
                     MessageTemplateAction(
                         label='六都',
