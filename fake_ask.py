8436db242fcedca82cc0ea05ab451740260f959d
smileakk <williamliao2013@gmail.com>
2023-09-26 22:57:14 +0800

    .

--- fake_ask.py
+++ fake_ask.py
@@ -46,13 +46,102 @@ def ask_Q1(event):
         template=CarouselTemplate(
             columns=[
                 CarouselColumn(
-                    imageAspectRatio='square',
+                    #thumbnail_image_url='https://img2.591.com.tw/house/2023/06/05/168596855727457664.jpg!510x400.jpg',
                     title='Q1',
                     text='附近有學校或教育機構嗎?',
                     actions=[
                         MessageTemplateAction(
                             label='詢問',
                             text='詢問 西屯區青海路二段193巷附近有學校或教育機構嗎?'
+                        ),
+                    ]
+                ),
+                CarouselColumn(
+                    #thumbnail_image_url='https://img2.591.com.tw/house/2023/06/05/168596855727457664.jpg!510x400.jpg',
+                    title='Q2',
+                    text='附近的公共交通工具方便嗎？',
+                    actions=[
+                        MessageTemplateAction(
+                            label='詢問',
+                            text='詢問 西屯區青海路二段193巷附近的公共交通工具方便嗎?'
+                        ),
+                    ]
+                ),
+                CarouselColumn(
+                    #thumbnail_image_url='https://img2.591.com.tw/house/2023/06/05/168596855727457664.jpg!510x400.jpg',
+                    title='Q3',
+                    text='附近有公園或是娛樂設施嗎？',
+                    actions=[
+                        MessageTemplateAction(
+                            label='詢問',
+                            text='詢問 西屯區青海路二段193巷附近有公園或是娛樂設施嗎?'
+                        ),
+                    ]
+                ),
+                CarouselColumn(
+                    #thumbnail_image_url='https://img2.591.com.tw/house/2023/06/05/168596855727457664.jpg!510x400.jpg',
+                    title='Q4',
+                    text='附近有運動地點或是健身中心嗎?',
+                    actions=[
+                        MessageTemplateAction(
+                            label='詢問',
+                            text='詢問 西屯區青海路二段193巷附近有運動地點或是健身中心嗎?'
+                        ),
+                    ]
+                ),
+                CarouselColumn(
+                    #thumbnail_image_url='https://img2.591.com.tw/house/2023/06/05/168596855727457664.jpg!510x400.jpg',
+                    title='Q5',
+                    text='附近是否有停車場或停車位？',
+                    actions=[
+                        MessageTemplateAction(
+                            label='詢問',
+                            text='詢問 西屯區青海路二段193巷附近是否有停車場或停車位?'
+                        ),
+                    ]
+                ),
+                CarouselColumn(
+                    #thumbnail_image_url='https://img2.591.com.tw/house/2023/06/05/168596855727457664.jpg!510x400.jpg',
+                    title='Q6',
+                    text='附近是否有醫院?',
+                    actions=[
+                        MessageTemplateAction(
+                            label='詢問',
+                            text='詢問 西屯區青海路二段193巷附近是否有醫院?'
+                        ),
+                    ]
+                ),
+                CarouselColumn(
+                    #thumbnail_image_url='https://img2.591.com.tw/house/2023/06/05/168596855727457664.jpg!510x400.jpg',
+                    title='Q7',
+                    text='附近有購物中心或商場嗎？',
+                    actions=[
+                        MessageTemplateAction(
+                            label='詢問',
+                            text='詢問 西屯區青海路二段193巷附近有購物中心或商場嗎?'
+                        ),
+                    ]
+                ),
+    
+            ]
+        )
+    )
+    line_bot_api.reply_message(event.reply_token,message)
+
+def ask_Q2(event):
+
+    message = TemplateSendMessage(
+        alt_text='Carousel template',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    imageAspectRatio='square',
+                    title='Q1',
+                    text='附近有超市或商店嗎?',
+                    actions=[
+                        MessageTemplateAction(
+                            label='詢問',
+                            text='詢問 西屯區青海路二段193巷附近有超市或商店嗎?'
                         ),
                     ]
                 ),
