6ef4aaa7a73a2d1626dac3f59a5525622a31b747
Your Name <stoy95536>
2023-09-26 14:51:25 +0800

    push_function_oline

--- app.py
+++ app.py
@@ -6,49 +6,14 @@ import os,datetime,openai,threading
 import pandas as pd
 import rent_house
 
-
 search_flag = 0
 event_data = []
-
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 openai.api_key = os.environ['CHATGPT_API_KEY']
-
-# @app.route("/")
-# def home():
-#   try:
-#     # 網址被執行時，等同使用 GET 方法發送 request，觸發 LINE Message API 的 push_message 方法
-#     line_bot_api.push_message('Uce078b5f96c3f0c0e76a9f0315f5b843', TextSendMessage(text='Hello World!!!'))
-#     return 'OK'
-#   except:
-#     print('error')
-    
-@app.route("/")
-def home():
-    global event_data
-    global search_flag
-    try:
-        if search_flag == 1:
-            event = event_data[0]
-            line_bot_api.push_message('Uce078b5f96c3f0c0e76a9f0315f5b843', TextSendMessage(text=search_flag))
-            Run_ChatGPT(event)
-            search_flag = 0
-            
-        msg = request.args.get('msg')   # 取得網址的 msg 參數
-        if msg != None:
-            # 如果有 msg 參數，觸發 LINE Message API 的 push_message 方法
-            line_bot_api.push_message('Uce078b5f96c3f0c0e76a9f0315f5b843', TextSendMessage(text=msg))
-            return msg
-        else:
-            return 'OK'
-
-    except:
-        print('error')
-        return 'error'
-        
 
 
 @app.route("/callback", methods=['POST'])
@@ -88,8 +53,6 @@ def handle_message(event): # event.message.text 使用者輸入內容
     # message = TextSendMessage(text=f"{type(event)} \n{event.source.user_id} \n{User_name} \n{User_name.display_name} ")
     # line_bot_api.reply_message(event.reply_token, message)
     
-    
-    
     if "查詢" in event.message.text :
         line_bot_api.push_message(f'{event.source.user_id}', TextSendMessage(text='請稍等...小Bee正在努力查詢中...'))
         
@@ -98,12 +61,7 @@ def handle_message(event): # event.message.text 使用者輸入內容
         except:
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!'))
         
-
         
-    if event.message.text == '你好':
-        message = TextSendMessage(text=f"你好\n時間{datetime.datetime.now()}") # bot return the Message to User
-        line_bot_api.reply_message(event.reply_token, message) 
-    
     if '找地圖' in event.message.text:
         location = event.message.text.split(" ")[1]
         message = TextSendMessage(text=f"https://www.google.com/maps/search/?api=1&query={location}\n時間{datetime.datetime.now()}") # bot return the Message to User
@@ -113,6 +71,7 @@ def handle_message(event): # event.message.text 使用者輸入內容
         tip = event.message.text.split(" ")[1]
         message = TextSendMessage(text=f"目前有符合您的項目喔\n時間{datetime.datetime.now()}") # bot return the Message to User
         line_bot_api.reply_message(event.reply_token, message)
+
 
     #以下是找租屋對話框
     if event.message.text == '找租屋':
