bb778fa2dc4f1742261e92db793f9f8d38a620e6
Your Name <stoy95536>
2023-09-26 14:32:11 +0800

    push_function_oline

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def home():
     try:
         if search_flag == 1:
             event = event_data[0]
-            line_bot_api.push_message('Uce078b5f96c3f0c0e76a9f0315f5b843', TextSendMessage(text=event))
+            line_bot_api.push_message('Uce078b5f96c3f0c0e76a9f0315f5b843', TextSendMessage(text=search_flag))
             Run_ChatGPT(event)
             search_flag = 0
             
